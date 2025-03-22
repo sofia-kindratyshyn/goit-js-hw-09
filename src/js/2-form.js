0fb764d3bfd3fca2cd94af9181bbeaf41f7d5fda
sofia-kindratyshyn <sofia25kind@gmail.com>
2025-03-22 21:08:03 +0200

    Added all files

---
+++ src/js/2-form.js
@@ -0,0 +1,33 @@
+const key = 'feedback-form-state';
+const form = document.querySelector('form');
+
+const savedData = JSON.parse(localStorage.getItem(key)) || {};
+const formdata = {
+  email: savedData.email ?? '',
+  message: savedData.message ?? '',
+};
+
+form.elements.email.value = formdata.email;
+form.elements.message.value = formdata.message;
+
+form.addEventListener('input', event => {
+  if (event.target.name === 'email' || event.target.name === 'message') {
+    formdata[event.target.name] = event.target.value;
+    localStorage.setItem(key, JSON.stringify(formdata));
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formdata.email.trim() || !formdata.message.trim()) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formdata);
+  localStorage.removeItem(key);
+  form.reset();
+  formdata.email = '';
+  formdata.message = '';
+});
